2071431f2234a33da719288927c52b299079e0bb
Achyut Shukla <achyut.shukla.btech2020@sitpune.edu.in>
2024-11-06 16:38:56 +0530

    feat: api gateway setup for spring microservice

---
+++ src/main/java/com/e_job_search/Api_Gateway/Application.java
@@ -0,0 +1,12 @@
+package com.e_job_search.Api_Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/e_job_search/Api_Gateway/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.e_job_search.Api_Gateway.exceptions;
+
+import jakarta.persistence.NonUniqueResultException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NonUniqueResultException.class)
+    public ResponseEntity<Map<String, Object>> nonUniqueResultException(NonUniqueResultException exception) {
+        Map<String, Object> errorDetail = new HashMap<>();
+        errorDetail.put("status", 409);
+        errorDetail.put("title", "Conflict");
+        errorDetail.put("description", "Duplicate records found");
+        errorDetail.put("message", exception.getMessage());
+
+        return ResponseEntity.status(409).body(errorDetail);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, Object>> illegalArgumentException(IllegalArgumentException exception) {
+        Map<String, Object> errorDetail = new HashMap<>();
+        errorDetail.put("status", 400);
+        errorDetail.put("title", "Bad Request");
+        errorDetail.put("description", "Invalid request parameters");
+        errorDetail.put("message", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorDetail);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> genericException(Exception exception) {
+        Map<String, Object> errorDetail = new HashMap<>();
+        errorDetail.put("status", 500);
+        errorDetail.put("title", "Internal Server Error");
+        errorDetail.put("description", "An error occurred while processing the request");
+        errorDetail.put("message", exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorDetail);
+    }
+}
